5edbe6b614744cc1633a8a09afcb882a533b11d4
filippocasari <256034@studenti.unimore.it>
2020-10-20 21:57:26 +0200

    aggiusto file Filippo

---
+++ Semaforo/Soluzione1Filippo/semaforoCasari.cpp
@@ -0,0 +1,99 @@
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+
+#endif
+
+#include <iostream>
+
+#ifndef NO_PI
+#include <wiringPi.h>
+#endif
+
+using namespace std;
+
+const int led_Blue = 0;
+const int timeoutMs = 1000; // 1 second
+
+const int stati[3] = {0, 1, 2};
+const int Time[3] = {2, 3, 2};
+void waitingTime(unsigned int *countPointer, int *tempo);
+
+int semaforo(unsigned int *countPointer, int *statocorrentePnt, int *tempo);
+
+///
+/// Utils
+///
+void init()
+{
+#ifndef NO_PI
+    wiringPiSetup();
+    pinMode(led_Blue, OUTPUT);
+#endif
+}
+
+void setLed(int ledNumber, bool value)
+{
+#ifndef NO_PI
+    digitalWrite(ledNumber, value);
+#else
+    cout << "Setting led " << ledNumber << " to " << (value ? "ON" : "OFF") << endl;
+#endif
+}
+
+int main()
+{
+    init();
+
+    unsigned int count = 0;
+    int statocorrente = stati[0];
+    int tempo = Time[0];
+    while (true)
+    {
+
+        cout << "stato corrente= " << statocorrente << endl;
+
+        statocorrente = semaforo(&count, &statocorrente, &tempo);
+    }
+    return 0;
+}
+int semaforo(unsigned int *countPointer, int *statocorrentePnt, int *tempo)
+{
+
+    switch (*statocorrentePnt)
+    {
+
+    case (0):
+
+        *statocorrentePnt = stati[1];
+        cout<<*statocorrentePnt;
+
+    case (1):
+        *statocorrentePnt = stati[2];
+
+    case (2):
+
+        *statocorrentePnt = stati[0];
+
+    }
+    *countPointer = 0;
+    *tempo = Time[*statocorrentePnt];
+    waitingTime(countPointer, tempo);
+
+    
+    return (*statocorrentePnt);
+}
+void waitingTime(unsigned int *countPointer, int *tempo)
+{
+    for (int i = 0; i < *tempo; i++)
+    {
+        *countPointer += 1;
+        cout << "sono passati... " << *countPointer << " sec" << endl;
+#ifndef NO_PI
+        delay(timeoutMs);
+#else
+        sleep(timeoutMs / 1000);
+#endif
+    }
+}
